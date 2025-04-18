fa789e0683e97796f2a16979789ab61752817cc9
Austin <ahunt@dcscorp.com>
2025-04-18 18:10:30 -0400

    added clear button, logged output for unimplemented button

--- calculator.js
+++ calculator.js
@@ -4,13 +4,23 @@ const calculator= document.querySelector(".calculator")
 
 const nonFunctionalButtons = ['0','1','2','3','4','5','6','7','8','9']
 
+const calculatorFunctions = {
+    'clear' : () => {
+        Display.value = ''
+    },
+}
 
 calculator.addEventListener("click", (event) => {
     if(event.target.tagName == 'BUTTON') {
         id = event.target.id
         if(id in nonFunctionalButtons) {
             Display.value = Display.value + id
+        } else {
+            try{
+                calculatorFunctions[id]()
+            } catch {
+                console.log(`Function ${id} isn't implemented yet`)
+            }
         }
-        console.log(id)
     }
 })
